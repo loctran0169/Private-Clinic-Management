02d3522a956bc0fee018ff22d58864912cb0dc25
Trần Hữu Lộc <34224781+loctran0169@users.noreply.github.com>
2019-05-15 22:48:06 +0700

    test 1

---
+++ Đồ Quán Quản Lý Phòng Mạch Tư/Đồ Quán Quản Lý Phòng Mạch Tư/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Đồ_Quán_Quản_Lý_Phòng_Mạch_Tư
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Đồ Quán Quản Lý Phòng Mạch Tư/Đồ Quán Quản Lý Phòng Mạch Tư/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.UserSkins;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+
+namespace Đồ_Quán_Quản_Lý_Phòng_Mạch_Tư
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            Application.Run(new Form1());
+        }
+    }
+}
