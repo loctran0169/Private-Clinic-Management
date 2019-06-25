0ab0d8c43418cf9fe167087a5481685bb05e3a7a
Trần Hữu Lộc <loctran0169@gmail.com>
2019-06-25 22:28:02 +0700

    Lộc- 3 cái DTO

---
+++ Đồ Quán Quản Lý Phòng Mạch Tư/QLPKDTO/BenhDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLPKDTO
+{
+    class BenhDTO
+    {
+        private string MaLB;
+        private string TenLoaiBenh;
+
+        public string MaLB1 { get => MaLB; set => MaLB = value; }
+        public string TenLoaiBenh1 { get => TenLoaiBenh; set => TenLoaiBenh = value; }
+    }
+}
---
+++ Đồ Quán Quản Lý Phòng Mạch Tư/QLPKDTO/PhieuKhamDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLPKDTO
+{
+    class PhieuKhamDTO
+    {
+        private string MaKP;
+        private string MaBN;
+        private string TrieuChung;
+        private string MaLN;
+        private DateTime NgayKham;
+
+        public string MaKP1 { get => MaKP; set => MaKP = value; }
+        public string MaBN1 { get => MaBN; set => MaBN = value; }
+        public string TrieuChung1 { get => TrieuChung; set => TrieuChung = value; }
+        public string MaLoaiBenh1 { get => MaLN; set => MaLN = value; }
+        public DateTime NgayKham1 { get => NgayKham; set => NgayKham = value; }
+    }
+}
