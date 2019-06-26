3d5a5389f6c1a423181cf917fc50ea643cc67a08
Trần Hữu Lộc <loctran0169@gmail.com>
2019-06-26 17:14:49 +0700

    Hùng - 3 lớp USERS, QuyenHan

---
+++ Đồ Quán Quản Lý Phòng Mạch Tư/QLPKDTO/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLPKDTO
+{
+    public class UserDTO
+    {
+        private int MaUS;
+        private int MaNV;
+        private int MaQH;
+        private string TaiKhoan;
+        private string MatKhau;
+
+        public int MaUS1 { get => MaUS; set => MaUS = value; }
+        public int MaNV1 { get => MaNV; set => MaNV = value; }
+        public int MaQH1 { get => MaQH; set => MaQH = value; }
+        public string TaiKhoan1 { get => TaiKhoan; set => TaiKhoan = value; }
+        public string MatKhau1 { get => MatKhau; set => MatKhau = value; }
+    }
+}
