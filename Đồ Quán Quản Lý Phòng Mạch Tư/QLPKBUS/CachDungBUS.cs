3add4c36febb685a7f8e599a4cc9785efcc61900
Trần Hữu Lộc <loctran0169@gmail.com>
2019-06-26 19:07:13 +0700

    hùng - cách dùng

---
+++ Đồ Quán Quản Lý Phòng Mạch Tư/QLPKBUS/CachDungBUS.cs
@@ -0,0 +1,46 @@
+﻿using QLPKDAL;
+using QLPKDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLPKBUS
+{
+    public class CachDungBUS
+    {
+        private CachDungDAL bdal;
+
+        public CachDungBUS()
+        {
+            bdal = new CachDungDAL();
+        }
+        public bool them(CachDungDTO lb)
+        {
+            bool re = bdal.them(lb);
+            return re;
+        }
+
+        public bool xoa(CachDungDTO lb)
+        {
+            bool re = bdal.xoa(lb);
+            return re;
+        }
+
+        public bool sua(CachDungDTO lb)
+        {
+            bool re = bdal.sua(lb);
+            return re;
+        }
+
+        public List<CachDungDTO> select()
+        {
+            return bdal.select();
+        }
+        public List<CachDungDTO> selectByKeyWord(string sKeyword)
+        {
+            return bdal.selectByKeyWord(sKeyword);
+        }
+    }
+}
