3a2d3977b9599007ae29a12af64fc09ec4e999ac
Khanh Nguyễn Trọng <17520627@ms.uit.edu.vn>
2019-06-30 13:49:19 +0700

    LAPDONTHUOC1

--- Đồ Quán Quản Lý Phòng Mạch Tư/Đồ Quán Quản Lý Phòng Mạch Tư/firmLapDonThuoc.cs
+++ Đồ Quán Quản Lý Phòng Mạch Tư/Đồ Quán Quản Lý Phòng Mạch Tư/firmLapDonThuoc.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using QLPKBUS;
+using QLPKDTO;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +21,34 @@ namespace Đồ_Quán_Quản_Lý_Phòng_Mạch_Tư
         public firmLapDonThuoc()
         {
             InitializeComponent();
+        }
+        private LapDonThuocBUS dtbus;
+        private void bt_load_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dtbus = new LapDonThuocBUS();
+            dt = dtbus.loadPKtoCombobox();
+
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                comboBox1.Items.Add(dt.Rows[i][1].ToString());
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dtbus = new LapDonThuocBUS();
+            List<LapPhieuKhamDTO> listphieukham = dtbus.select();
+            if (listphieukham == null)
+                MessageBox.Show("Load danh sách bệnh nhân thất bại. Vui lòng kiểm tra đường truyền");
+            else
+            {
+                MessageBox.Show("Load bệnh nhân thành công");
+                bt_sua.Visible = true;
+                bt_xoa.Visible = true;
+            }
+            loadDsPK(listphieukham);
         }
     }
 }
