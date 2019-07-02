d6665338e6f985b3fe9b7999b20311c8fef9cfd8
Trần Hữu Lộc <loctran0169@gmail.com>
2019-07-02 15:54:29 +0700

    fix báo cáo + hóa đơn

---
+++ Đồ Quán Quản Lý Phòng Mạch Tư/Đồ Quán Quản Lý Phòng Mạch Tư/rpHoaDon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Đồ_Quán_Quản_Lý_Phòng_Mạch_Tư
+{
+    public partial class rpHoaDon : DevExpress.XtraReports.UI.XtraReport
+    {
+        public rpHoaDon()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
